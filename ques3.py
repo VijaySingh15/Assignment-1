f5853f24514308ab2989067e8b992ba1eb47708d
VijaySingh15 <110224104+VijaySingh15@users.noreply.github.com>
2022-09-05 19:21:13 +0530

    Add files via upload

---
+++ ques3.py
@@ -0,0 +1,6 @@
+a=5
+b=10
+c=15
+print(a,b)
+print("hello")
+print(c)
---
+++ ques9.py
@@ -0,0 +1,5 @@
+name="Vijay Singh"
+age=20
+qualification="Twelth Pass"
+experience="one year's experience"
+print(name,age,qualification,experience,sep=',')
